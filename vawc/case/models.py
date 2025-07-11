e8f7e01132a3ed9c492b46bbb159500eea2bd903
coffee-Maker19 <125376601+coffee-Maker19@users.noreply.github.com>
2025-07-11 18:15:15 +0800

    Update: Added Others in Referral Actions for law_enforcement refferal and error handling atleast 1 Referral Actions when reffering

--- vawc/case/models.py
+++ vawc/case/models.py
@@ -116,6 +116,7 @@ class Case(models.Model):
     rescue_operations_of_vaw_cases = models.BooleanField(default=False)
     forensic_interview_and_investigation = models.BooleanField(default=False)
     enforcement_of_protection_order = models.BooleanField(default=False)
+    law_enforcement_others = models.BooleanField(default=False)
     remarks_to_law_enforcement = models.TextField(null=True, blank=True)
     remarks_to_barangay = models.TextField(null=True, blank=True)
 
--- vawc/pages/views.py
+++ vawc/pages/views.py
@@ -4011,11 +4011,14 @@ def process_service_info(request):
         case.rescue_operations_of_vaw_cases = True if request.POST.get('resuce_operation') == 'true' else False
         case.forensic_interview_and_investigation = True if request.POST.get('forensic_interview') == 'true' else False
         case.enforcement_of_protection_order = True if request.POST.get('enforce_protect_order') == 'true' else False
+        case.law_enforcement_others = True if request.POST.get('law_enforcement_others') == 'true' else False
+
         if( request.POST.get('remarks_law_enforcement')):
             case.remarks_to_law_enforcement = request.POST.get('remarks_law_enforcement', '')
         if( request.POST.get('remarks_barangay')):
             case.remarks_to_barangay = request.POST.get('remarks_barangay', '')
         case.refer_to_barangay_date = parse_date(request.POST.get('today', ''))
+        
 
         case.refers_to_other_service_provider = True if request.POST.get('refer_other_service') == 'true' else False
         case.refer_other_service_date = parse_date(request.POST.get('refer_other_service_date', ''))
